db25dc5fa420ffec18abbadfb6b1d3fa4c9c9a27
DURU <enfn2001@gmail.com>
2019-11-21 22:10:28 +0900

    jenkins test

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -26,3 +26,13 @@ pipeline {
     }
   }
 }
+// node 키워드로 파이프라인을 정의하면, script {} 없이 if-else와 같은 스크립트 작성 가능
+node {
+  stage('Example') {
+      if (env.BRANCH_NAME == 'master') {
+          echo 'I only execute on the master branch'
+      } else {
+          echo 'I execute elsewhere'
+      }
+  }
+}

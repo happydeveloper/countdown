2c31d5d8538cb601a86ab4a3a0fc80e6d4e1fd20
duru <enfn2001@gmail.com>
2020-04-28 15:19:15 +0900

    M src/util.js - diff --git a/src/util.js b/src/util.js index c017b21..8ff1011 100644 --- a/src/util.js +++ b/src/util.js @@ -16,4 +16,40 @@ function getDestTime(duration){     return date.getHours()+":"+('0'+date.getMinutes()).slice(-2);  }
    +
    +// 전체화면 설정
    +var bgDoc = document.documentElement;
    +
    +function openFullScreenMode() {
    +    if (bgDoc.requestFullscreen) {
    +        const p = el.webkitRequestFullScreen();
    +    p.then(() => {console.log('full screen')});
    +        bgDoc.requestFullscreen();
    +    } else if (bgDoc.webkitRequestFullscreen) { // Chrome, Safari (webkit)
    +        bgDoc.webkitRequestFullscreen();
    +    } else if (bgDoc.mozRequestFullScreen) { // Firefox
    +        bgDoc.mozRequestFullScreen();
    +    } else if (bgDoc.msRequestFullscreen) { // IE or Edge
    +        bgDoc.msRequestFullscreen();
    +    }
    +}
    +
    +var elem = document.documentElement;
    +function openFullscreen() {
    +    if(!document.fullscreenElement) {
    +        if (elem.requestFullscreen) {
    +            elem.requestFullscreen().catch(err=>{
    +                console.log(`Error attempting to enable full-screen mode: ${err.message} (${err.name})`);
    +                openFullscreen();
    +            });
    +          } else if (elem.mozRequestFullScreen) { /* Firefox */
    +            elem.mozRequestFullScreen();
    +          } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
    +            elem.webkitRequestFullscreen();
    +          } else if (elem.msRequestFullscreen) { /* IE/Edge */
    +            elem.msRequestFullscreen();
    +          }
    +    }
    +}
    +
     export default util
    \ No newline at end of file - 2020년 4월 28일 화요일 15시 19분 15초 KST

--- src/util.js
+++ src/util.js
@@ -16,4 +16,40 @@ function getDestTime(duration){
    return date.getHours()+":"+('0'+date.getMinutes()).slice(-2);
 }
 
+
+// 전체화면 설정
+var bgDoc = document.documentElement;
+
+function openFullScreenMode() {    
+    if (bgDoc.requestFullscreen) {
+        const p = el.webkitRequestFullScreen();
+    p.then(() => {console.log('full screen')});
+        bgDoc.requestFullscreen();
+    } else if (bgDoc.webkitRequestFullscreen) { // Chrome, Safari (webkit)
+        bgDoc.webkitRequestFullscreen();
+    } else if (bgDoc.mozRequestFullScreen) { // Firefox
+        bgDoc.mozRequestFullScreen();
+    } else if (bgDoc.msRequestFullscreen) { // IE or Edge
+        bgDoc.msRequestFullscreen();
+    }
+}
+
+var elem = document.documentElement;
+function openFullscreen() {
+    if(!document.fullscreenElement) {
+        if (elem.requestFullscreen) {
+            elem.requestFullscreen().catch(err=>{
+                console.log(`Error attempting to enable full-screen mode: ${err.message} (${err.name})`);
+                openFullscreen();
+            });
+          } else if (elem.mozRequestFullScreen) { /* Firefox */
+            elem.mozRequestFullScreen();
+          } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
+            elem.webkitRequestFullscreen();
+          } else if (elem.msRequestFullscreen) { /* IE/Edge */
+            elem.msRequestFullscreen();
+          }
+    }
+}
+
 export default util

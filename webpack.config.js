c839b498f90e7c73bc904486d09c1a4a4b473c8d
duru <enfn2001@gmail.com>
2020-04-29 04:48:03 +0900

    M webpack.config.js - diff --git a/webpack.config.js b/webpack.config.js index 1290779..92fdd07 100644 --- a/webpack.config.js +++ b/webpack.config.js @@ -24,7 +24,7 @@ module.exports = {              },              {                  test: /\.scss$/, -                loader: 'style-loader!css-loader!sass-loader' +                loader: 'style-loader!css-loader!sass-loader',                  use: ['style-loader','css-loader']              }          ] - 2020년 4월 29일 수요일 04시 48분 03초 KST

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
             },
             {
                 test: /\.scss$/,
-                loader: 'style-loader!css-loader!sass-loader'
+                loader: 'style-loader!css-loader!sass-loader',
                 use: ['style-loader','css-loader']
             }
         ]
